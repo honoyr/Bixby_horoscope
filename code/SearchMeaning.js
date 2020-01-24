93946eb9fd380e3041e491cc7de3397488c3200f
den <denis.gonor@gmail.com>
2020-01-23 21:07:51 -0800

    updated

--- code/SearchMeaning.js
+++ code/SearchMeaning.js
@@ -10,7 +10,7 @@ module.exports.function = function searchMeaning (word) {
   var meaningId = searchWordResponse[0].meanings[0].id;
   var db_name = "bixby_jdv";
   var ContactId = 121212;
-  var myAPIKEY = 'jc13HjwDduIPNOgu1tSxz3BT_CNuDx4H';
+  var myAPIKEY = 'fa8e615af3mshe43bc1e20b3e43fp1a0bd1jsn5e67a3865edd';
 
   var params = {
     'id_word': meaningId
@@ -19,7 +19,12 @@ module.exports.function = function searchMeaning (word) {
     format: 'json',
     passAsJson: true,
   };
-    var postWord = http.postUrl("https://api.mlab.com/api/1/databases/" + db_name + "/collections/" + ContactId + '?apiKey=' + myAPIKEY, params, options);
+  
+  // curl -X POST --include 'https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aries&day=today' \
+  // -H 'X-RapidAPI-Host: sameer-kumar-aztro-v1.p.rapidapi.com' \
+  // -H 'X-RapidAPI-Key: fa8e615af3mshe43bc1e20b3e43fp1a0bd1jsn5e67a3865edd' \
+  // -H 'Content-Type: application/x-www-form-urlencoded'
+    var postWord = http.postUrl(config.get('remote.url')" + db_name + "/collections/" + ContactId + '?apiKey=' + myAPIKEY, params, options);
   console.log(postWord)
   
   // If id is "1111", then this makes a GET call to /meanings?ids=1111
